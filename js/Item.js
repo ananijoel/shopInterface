bf76ac2d17692ff5c7f74078e5ae416275166098
ananijoel <ananijoellanatide@gmail.com>
2024-07-16 09:50:05 +0000

    mise en place du responsive

--- js/Item.js
+++ js/Item.js
@@ -1,4 +1,5 @@
 import {convertirDate} from './functions.js'
+import {fetchAPI} from '../js/Category.js'
 const urlString = window.location.href;
 const url = new URL(urlString);
 const params = new URLSearchParams(url.search);
@@ -7,7 +8,7 @@ const matricule_value = params.get('data');
 let Item_pictures = document.querySelector('.Item_pictures');
 let Main_Item_picture = document.querySelector('.Main_Item_picture');
 let Item_Informations = document.querySelector('.Item_Informations');
-let Other_Items = document.querySelector('.Other_Items');
+let Item_container = document.querySelector('.Item_container');
 
 let nom = document.querySelector('.name');
 let creation = document.querySelector('.date');
@@ -16,18 +17,14 @@ let types = document.querySelector('.types');
 let quantity = document.querySelector('.quantity input');
 let prix = document.querySelector('.prix p:nth-child(2)');
 let button = document.querySelector('.button');
+let pictures = document.querySelectorAll('.img')
+let mainPicture = document.querySelector('.Main_Item_picture img')
+let options = document.querySelectorAll('.buttonOption')
+
 /*
 
-
-
-
-
-
-let alltypes = ''
-let options = document.querySelectorAll('.buttonOption')
 document.querySelector('.boutique').classList.add('active')
-let pictures = document.querySelectorAll('.img')
-let mainPicture = document.querySelector('.image img')
+
 */
 fetch(`https://node-game-store-api-e25abde5221a.herokuapp.com/api/Item/${matricule_value}`)
     .then(response => response.json())
@@ -49,4 +46,24 @@ fetch(`https://node-game-store-api-e25abde5221a.herokuapp.com/api/Item/${matricu
         };
         updatePrixTotal();
         quantity.addEventListener('input', updatePrixTotal);
+        fetchAPI('category',data.category[0],'10',Item_container)
     })
+
+pictures.forEach(picture => {
+    picture.addEventListener('click', (e) => {
+        // Supprimer la classe 'active' de tous les éléments
+        pictures.forEach(p => p.classList.remove('active'));
+        
+        // Ajouter la classe 'active' à l'élément cliqué
+        picture.classList.add('active');
+        mainPicture.setAttribute('src', picture.getAttribute('src'))
+    });
+});
+
+options.forEach(option =>{
+    option.addEventListener('click', (e) =>{
+        options.forEach(o =>o.classList.remove('active'))
+        option.classList.toggle('active')
+    })
+})
+
